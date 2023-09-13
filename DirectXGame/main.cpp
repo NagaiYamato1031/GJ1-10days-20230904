355397e71c8126a20577dd807c6ffc630a9cbee0
大陸 永井 <k022g0053@m.neec.ac.jp>
2023-09-13 23:42:31 +0900

    タイトル変更

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -24,7 +24,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2A_12_ナガイ_ヤマト");
+	win->CreateGameWindow(L"2008_とべ!!ソーセージ");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
